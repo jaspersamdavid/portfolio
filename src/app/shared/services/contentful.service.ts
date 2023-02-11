336216b3b672a7a4b81977435fb9d45b1690dd7d
jaspersamdavid <43448736+jaspersamdavid@users.noreply.github.com>
2023-02-10 23:01:14 -0500

    Add files via upload
    Added base code setup for portfolio

---
+++ src/app/shared/services/contentful.service.ts
@@ -0,0 +1,44 @@
+import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import {
+  ContentfulClientApi,
+  createClient,
+} from 'contentful';
+import { from } from 'rxjs';
+import * as marked from 'marked';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ContentfulService {
+
+  private clientApi: ContentfulClientApi;
+
+  constructor() {
+    this.clientApi = createClient({
+      space: environment.contentful.spaceId,
+      accessToken: environment.contentful.token,
+    });
+  }
+
+  getDetails(item) {
+    const promise = this.clientApi.getEntries(
+      Object.assign({
+        content_type: item
+      }))
+    // const promise1 = {
+    //   about: [{
+    //     skills: 'angular'
+    //   }]
+    // };
+    // const promise = promise1[item];
+    return from(promise).pipe(map(entry => entry.items.map(i => i.fields)))
+  }
+
+  markdownToHtml(md: string) {
+    return marked(md);
+  }
+
+}
